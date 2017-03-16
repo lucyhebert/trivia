37827769229e6003e8f74a2cc22765f5d753c0d3
HEBERT LUCY p1614402 <p1614402@univ-lyon1.fr>
2017-03-16 17:51:59 +0100

    Refactoring

---
+++ C#/Trivia/Trivia.Tests/GoldenMaster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+
+namespace Trivia.Tests
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    public class GoldenMaster
+    {
+        [Test]
+        public void ShouldNotChange()
+        {
+            var stringWriter = new StringWriter();
+            var previouConsoleOut = Console.Out;
+            Console.SetOut(stringWriter);
+            GameRunner.Main(null);
+            Console.SetOut(previouConsoleOut);
+            Approvals.Verify(stringWriter.ToString());
+        }
+    }
+}
---
+++ C#/Trivia/Trivia/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Trivia
+{
+    public class Player
+    {
+        public String PlayerName { get; set; }
+        public int Place { get; set; }
+        public int Purse { get; set; }
+        public bool InPenaltyBox { get; set; }
+
+        public Player(String name)
+        {
+            this.PlayerName = name;
+            this.Place = 0;
+            this.Purse = 0;
+            this.InPenaltyBox = false;
+        }
+
+    }
+}

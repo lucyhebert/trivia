bec66d5dd72c262708e0b964dca66e842a09cd7f
Lucy <Lucy@PC_LUCY>
2017-06-22 13:17:04 +0200

    Add Web API

---
+++ C#/Trivia/Trivia.WebAPI/HelloModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Trivia.WebAPI
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+        }
+    }
+}
---
+++ C#/Trivia/Trivia.WebAPI/TriviaModule.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy.ModelBinding;
+using Trivia.DataAccess;
+using Trivia.Domain;
+using Trivia.Presentation;
+
+namespace Trivia.WebAPI
+{
+    public class TriviaModule : Nancy.NancyModule
+    {
+        public TriviaModule()
+        {
+            Post["/newGame"] = NewGame;
+        }
+
+        private dynamic NewGame(dynamic o)
+        {
+            var newGame = this.Bind<NewGame>();
+
+            var repository = new QuestionsFromScratchRepository();
+            var questionUi = new WebUI();
+
+            var players = new Players(questionUi);
+
+            foreach (var userName in newGame.UserNames)
+            {
+                players.AddAPlayer(userName);
+            }
+
+            List<string> categories = newGame.QuestionCategories;
+
+            var questions = new Questions(categories, repository);
+
+            var aGame = new Game(players, questions, questionUi);
+            
+            return questionUi.Message;
+        }
+    }
+
+    internal class WebUI : IQuestionUI
+    {
+
+        public void Display(string message)
+        {
+            Message += message + "\n";
+        }
+
+        public string Message { get; set; }
+    }
+
+    public class NewGame
+    {
+        public List<string> UserNames { get; set; }
+        public List<string> QuestionCategories { get; set; }
+    }
+
+}

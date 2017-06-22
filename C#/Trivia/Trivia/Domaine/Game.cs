ed79c9539ea42fb6e8efd10da9956721aadad201
Lucy <Lucy@PC_LUCY>
2017-06-22 10:25:06 +0200

    Create folders in project

---
+++ C#/Trivia/Trivia/Domaine/Game.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Trivia
+{
+    public class Game
+    {
+        private readonly Players _players;
+        private readonly Questions _questions;
+        private readonly IQuestionUI _questionUi;
+
+        public bool GoOutFromPenaltyBox;
+
+        public Game(Players players, Questions questions, IQuestionUI questionUi) {
+            _players = players;
+            _questions = questions;
+            _questionUi = questionUi;
+        }
+
+        public void Roll(int roll)
+        {
+            string[] question = null;
+            _questionUi.Display(_players.CurrentPlayer.PlayerName + " is the current player");
+            _questionUi.Display("They have rolled a " + roll);
+
+            if (_players.CurrentPlayer.IsInPenaltyBox)
+            {
+                if (roll % 2 != 0)
+                {
+                    GoOutFromPenaltyBox = true;
+                    _questionUi.Display(_players.CurrentPlayer.PlayerName + " is getting out of the penalty box");
+                    _players.CurrentPlayer.Move(roll);
+
+                    _questionUi.Display(_players.CurrentPlayer.PlayerName
+                                  + "'s new location is "
+                                  + _players.CurrentPlayer.Place);
+                    question = _questions.AskQuestion(_players.CurrentPlayer.Place);
+                    _questionUi.Display("The category is " + question[0]);
+                    _questionUi.Display(question[1]);
+                }
+                else
+                {
+                    GoOutFromPenaltyBox = false;
+                    _questionUi.Display(_players.CurrentPlayer.PlayerName + " is not getting out of the penalty box");
+                }
+            }
+            else
+            {
+                _players.CurrentPlayer.Move(roll);
+                _questionUi.Display(_players.CurrentPlayer.PlayerName
+                                  + "'s new location is "
+                                  + _players.CurrentPlayer.Place);
+                question = _questions.AskQuestion(_players.CurrentPlayer.Place);
+                _questionUi.Display("The category is " + question[0]);
+                _questionUi.Display(question[1]);
+            }
+        }
+
+        public bool WasCorrectlyAnswered()
+        {
+            //if (_players.CurrentPlayer.IsInPenaltyBox)
+            //{
+            //    _questionUi.Display("Answer was correct!!!!");
+            //    _players.CurrentPlayer.WinAGoldCoin();
+            //    _players.NextPlayer();
+            //    return DidPlayerWin();
+            //}
+            //else
+            //{
+            //    _players.NextPlayer();
+            //    return true;
+            //}
+
+            bool winner;
+
+            if (_players.CurrentPlayer.IsInPenaltyBox)
+            {
+                if (GoOutFromPenaltyBox)
+                {
+                    _questionUi.Display("Answer was correct!!!!");
+                    _players.CurrentPlayer.WinAGoldCoin();
+
+                    winner = _players.CurrentPlayer.IsWinner();
+                    _players.NextPlayer();
+
+                    return winner;
+                }
+
+                _players.NextPlayer();
+                return false;
+
+            }
+
+            _questionUi.Display("Answer was corrent!!!!");
+            _players.CurrentPlayer.WinAGoldCoin();
+
+            winner = _players.CurrentPlayer.IsWinner();
+            _players.NextPlayer();
+
+            return winner;
+
+        }
+
+
+        public bool WrongAnswer()
+        {
+            _questionUi.Display("Question was incorrectly answered");
+            _questionUi.Display(_players.CurrentPlayer.PlayerName + " was sent to the penalty box");
+            _players.CurrentPlayer.GoToPenaltyBox();
+
+            _players.NextPlayer();
+            return false;
+        }
+
+        private bool DidPlayerWin()
+        {
+            return _players.CurrentPlayer.GoldCoins != 6;
+        }
+    }
+
+}
---
+++ C#/Trivia/Trivia/Domaine/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Trivia
+{
+    public class Player
+    {
+        public string PlayerName { get; set; }
+        public int Place { get; set; }
+        public int GoldCoins { get; set; }
+        public bool IsInPenaltyBox { get; set; }
+        public IQuestionUI QuestionUi { get; set; }
+
+        public Player(string name, IQuestionUI questionUi)
+        {
+            PlayerName = name;
+            Place = 0;
+            GoldCoins = 0;
+            IsInPenaltyBox = false;
+            QuestionUi = questionUi;
+        }
+
+
+        public void Move(int roll)
+        {
+            Place = Place + roll;
+            if (Place > 11) Place = Place - 12;
+        }
+
+        public void WinAGoldCoin()
+        {
+            GoldCoins++;
+            QuestionUi.Display(PlayerName
+                              + " now has "
+                              + GoldCoins
+                              + " Gold Coins.");
+        }
+
+        public void GoToPenaltyBox()
+        {
+            IsInPenaltyBox = true;
+        }
+
+        //public void GoOutFromPenaltyBox()
+        //{
+        //    IsInPenaltyBox = false;
+        //}
+
+        public bool IsWinner()
+        {
+            return GoldCoins == 6;
+        }
+    }
+}
